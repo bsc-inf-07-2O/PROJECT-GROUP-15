fbf014c25bb799a7324d948f4eceb556c0e11af6
bsc-inf-07-2O <bsc-inf-07-20@unima.ac.mw>
2024-11-09 00:50:03 +0200

    backend

---
+++ src/components/password/reset.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+ 
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-800">
+      
+    </div>
+  );
+};
+
+export default ForgotPassword;
