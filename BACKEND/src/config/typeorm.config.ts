0154a627c53d982cf0a791e19cd6274398304ecb
JOELGANIZANI75 <bsc-inf-12-22@unima.ac.mw>
2024-11-21 14:06:25 +0200

    config foder

---
+++ BACKEND/src/config/typeorm.config.ts
@@ -0,0 +1,37 @@
+import { TypeOrmModuleOptions, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { User } from '../users/Student.entity';
+import { Bonding } from '../bonding/bonding-entity';
+import { Eligible } from 'src/erigible/erigibility-entity';
+import {University} from '../university/University.entity';
+
+
+// Async TypeORM configuration using environment variables
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    const dbPort = parseInt(process.env.DB_PORT || '5433', 10);
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbUsername = process.env.DB_USERNAME || 'defaultUser';
+    const dbPassword = process.env.DB_PASSWORD || 'defaultPassword';
+    const dbName = process.env.DB_NAME || 'defaultDB';
+
+    return {
+      type: 'postgres',
+      host: dbHost,
+      port: dbPort,
+      username: dbUsername,
+      password: dbPassword,
+      database: dbName,
+      entities: [User, Bonding, Eligible, University],
+      migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+      extra: {
+        charset: 'utf8mb4_unicode_ci',
+      },
+      synchronize: true, // Be cautious with this in production; use migrations instead
+      logging: true,
+      connectTimeoutMS: 30000, 
+    };
+  },
+};
+
+// Static TypeORM configuration
+
