98167e59db308b3dd6f069eb4ba76077a76475e1
JOELGANIZANI75 <bsc-inf-12-22@unima.ac.mw>
2024-11-24 21:04:28 +0200

    bonding controller,dto, module

--- BACKEND/src/bonding/bonding.module.ts
+++ BACKEND/src/bonding/bonding.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { BondingService } from './bonding.service';
 import { BondingController } from './bonding.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bonding } from './bonding-entity';
+import { UsersModule } from '../users/users.module';
+import { UniversityModule } from '../university/university.module';
+import { User } from '../users/Student.entity';
+import { University } from '../university/University.entity';
+import { mailerModule } from '../mailer/mailer.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Bonding, User, University]),
+    UsersModule,
+    UniversityModule,
+    mailerModule,
+    
+  ],
   providers: [BondingService],
-  controllers: [BondingController]
+  controllers: [BondingController],
 })
 export class BondingModule {}
