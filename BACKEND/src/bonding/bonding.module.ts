f678cf77df6de45b6b625c1363b4844ce94be8a3
“dumi299” <bsc-com-14-21@unima.ac.mw>
2024-11-25 23:57:08 +0200

    Authentication

--- BACKEND/src/bonding/bonding.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BondingService } from './bonding.service';
-import { BondingController } from './bonding.controller';
-
-@Module({
-  providers: [BondingService],
-  controllers: [BondingController]
-})
-export class BondingModule {}
