673f579a18f0397fd33c0259c9e66638dc5c42d5
mrkapakasa <167528154+mrkapakasa@users.noreply.github.com>
2024-11-26 13:15:12 +0200

    front end

--- FRONTEND/src/components/Axio.js
+++ FRONTEND/src/components/Axio.js
@@ -8,7 +8,7 @@ const Axio = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:3001/api/data');
+                const response = await axios.get('http://localhost:3000/api/data');
                 setData(response.data);
             } catch (error) {
                 console.error('Error fetching data', error);
